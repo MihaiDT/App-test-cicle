e03977700874b3dec46a12d8755aec1632fd8ecc
Danilo Spanò <d.spano@tandu.it>
2024-01-05 11:46:14 +0100

    fix: fixed user model

--- lib/data/models/user.dart
+++ lib/data/models/user.dart
@@ -1,76 +1,80 @@
 class User {
-  String? birthdate;
-  String? email;
-  String? firstName;
-  bool? isConfirmed;
-  String? lastMenstrautionDate;
-  String? lastName;
-  String? legalGuardianEmail;
-  String? mgmCode;
-  String? nickname;
-  String? password;
-  int? periodDays;
-  int? periodDuration;
-  bool? privacyProfiling;
-  bool? privacyMarketingEmail;
-  String? provider;
-  String? sessionToken;
+  final String birthdate;
+  final String email;
+  final String firstName;
+  final bool isConfirmed;
+  final String? lastMenstrautionDate;
+  final String lastName;
+  final String? legalGuardianEmail;
+  final String? mgmCode;
+  final String? nickname;
+  final String? password;
+  final int periodDays;
+  final int periodDuration;
+  final bool? privacyProfiling;
+  final bool? privacyMarketingEmail;
+  final String provider;
+  final String sessionToken;
 
-  User(
-      {this.birthdate,
-      this.email,
-      this.firstName,
-      this.isConfirmed,
-      this.lastMenstrautionDate,
-      this.lastName,
-      this.legalGuardianEmail,
-      this.mgmCode,
-      this.nickname,
-      this.password,
-      this.periodDays,
-      this.periodDuration,
-      this.privacyProfiling,
-      this.privacyMarketingEmail,
-      this.provider,
-      this.sessionToken});
+  User({
+    required this.birthdate,
+    required this.email,
+    required this.firstName,
+    required this.isConfirmed,
+    this.lastMenstrautionDate,
+    required this.lastName,
+    this.legalGuardianEmail,
+    required this.mgmCode,
+    this.nickname,
+    this.password,
+    required this.periodDays,
+    required this.periodDuration,
+    this.privacyProfiling,
+    this.privacyMarketingEmail,
+    required this.provider,
+    required this.sessionToken,
+  });
 
-  User.fromJson(Map<String, dynamic> json) {
-    birthdate = json['birthdate'];
-    email = json['email'];
-    firstName = json['first_name'];
-    isConfirmed = json['is_confirmed'];
-    lastMenstrautionDate = json['last_menstraution_date'];
-    lastName = json['last_name'];
-    legalGuardianEmail = json['legal_guardian_email'];
-    mgmCode = json['mgm_code'];
-    nickname = json['nickname'];
-    password = json['password'];
-    periodDays = json['period_days'];
-    periodDuration = json['period_duration'];
-    privacyProfiling = json['privacy_profiling'];
-    privacyMarketingEmail = json['privacy_marketing_email'];
-    provider = json['provider'];
-    sessionToken = json['session_token'];
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      birthdate: json['user']['birthdate'],
+      email: json['user']['email'],
+      firstName: json['user']['first_name'],
+      isConfirmed: json['user']['is_confirmed'],
+      lastMenstrautionDate: json['user']['last_menstraution_date'],
+      lastName: json['user']['last_name'],
+      mgmCode: json['user']['mgm_code'],
+      periodDays: json['user']['period_days'],
+      periodDuration: json['user']['period_duration'],
+      privacyProfiling: json['user']['privacy_profiling'],
+      privacyMarketingEmail: json['user']['privacy_marketing_email'],
+      provider: json['user']['provider'],
+      sessionToken: json['user']['session_token'],
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['birthdate'] = this.birthdate;
-    data['email'] = this.email;
-    data['first_name'] = this.firstName;
-    data['is_confirmed'] = this.isConfirmed;
-    data['last_menstraution_date'] = this.lastMenstrautionDate;
-    data['last_name'] = this.lastName;
-    data['legal_guardian_email'] = this.legalGuardianEmail;
-    data['mgm_code'] = this.mgmCode;
-    data['nickname'] = this.nickname;
-    data['password'] = this.password;
-    data['period_days'] = this.periodDays;
-    data['period_duration'] = this.periodDuration;
-    data['privacy_profiling'] = this.privacyProfiling;
-    data['privacy_marketing_email'] = this.privacyMarketingEmail;
-    data['provider'] = this.provider;
-    data['session_token'] = this.sessionToken;
-    return data;
+    return {
+      "birthdate": birthdate,
+      "email": email,
+      "first_name": firstName,
+      "is_confirmed": isConfirmed,
+      "last_menstraution_date": lastMenstrautionDate,
+      "last_name": lastName,
+      "legal_guardian_email": legalGuardianEmail,
+      "mgm_code": mgmCode,
+      "nickname": nickname,
+      "password": password,
+      "period_days": periodDays,
+      "period_duration": periodDuration,
+      "privacy_profiling": privacyProfiling,
+      "privacy_marketing_email": privacyMarketingEmail,
+      "session_token": sessionToken,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'User{birthdate: $birthdate, email: $email, firstName: $firstName, isConfirmed: $isConfirmed, lastMenstrautionDate: $lastMenstrautionDate, lastName: $lastName, legalGuardianEmail: $legalGuardianEmail, mgmCode: $mgmCode, nickname: $nickname, password: $password, periodDays: $periodDays, periodDuration: $periodDuration, privacyProfiling: $privacyProfiling, privacyMarketingEmail: $privacyMarketingEmail, provider: $provider, sessionToken: $sessionToken}';
   }
 }
