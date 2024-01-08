cdbf59ab189ed7f1a3f22ce43ddbaae0fabd71a9
Danilo Spanò <d.spano@tandu.it>
2024-01-08 11:05:25 +0100

    feat: added method _saveBirthDate

--- lib/modules/birth_date/birth_date_page.dart
+++ lib/modules/birth_date/birth_date_page.dart
@@ -2,14 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:lines/core/theme/text_wrapper.dart';
-import 'package:lines/core/theme/theme_button_size.dart';
 import 'package:lines/core/theme/theme_decoration.dart';
 import 'package:lines/core/theme/theme_size.dart';
 import 'package:lines/core/theme/theme_sized_box.dart';
+import 'package:lines/core/utils/singletons.dart';
 import 'package:lines/modules/birth_date/birth_date_controller.dart';
 import 'package:lines/routes/routes.dart';
 import 'package:lines/widgets/appbar/transparent_app_bar.dart';
-import 'package:lines/widgets/buttons/primary_button.dart';
 import 'package:lines/widgets/buttons/secondary_button.dart';
 import 'package:lines/widgets/forms/input_text_field.dart';
 import 'package:lines/widgets/layouts/app_scaffold_page.dart';
@@ -43,6 +42,11 @@ class BirthDatePage extends GetView<BirthDateController> {
           child: SecondaryButton(
             text: "AVANTI",
             onPressed: () {
+              _saveBirthDate(
+                controller.dayController.value.text,
+                controller.monthController.value.text,
+                controller.yearController.value.text,
+              );
               Get.toNamed(Routes.privacy);
             },
           ),
@@ -116,5 +120,10 @@ class BirthDatePage extends GetView<BirthDateController> {
         ),
       ),
     );
+  }
+
+  /// Save birth date in App state
+  void _saveBirthDate(String day, String month, String year) {
+    appController.user.value?.birthdate = "$year-$month-$day";
   }
 }
