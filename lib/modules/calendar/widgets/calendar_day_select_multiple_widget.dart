2dc63135073b10927217525347336982d331257a
Giacomo Damosso <g.damosso@tandu.it>
2024-01-30 13:22:30 +0000

    Merged in feature/calendar_day_select_multiple_widget (pull request #51)
    feat : created calendar day select multiple widget
    
    * added calendar_day_select_multiple_widget file and class
    
    * added callback
    
    * refactor
    
    * removed unnecessary field
    
    * format
    
    
    Approved-by: Danilo Spanò

---
+++ lib/modules/calendar/widgets/calendar_day_select_multiple_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gradient_borders/box_borders/gradient_box_border.dart';
+import 'package:lines/core/app_theme.dart';
+
+class CalendarDaySelectMultipleWidget extends StatelessWidget {
+  final bool isSelected;
+  final String text;
+  final bool isToday;
+
+  final VoidCallback? callback;
+
+  const CalendarDaySelectMultipleWidget({
+    required this.isSelected,
+    required this.text,
+    required this.isToday,
+    this.callback,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: callback,
+      child: Column(
+        children: [
+          BodyLarge(
+            text,
+            textAlign: TextAlign.center,
+            fontWeight: isToday
+                ? NewThemeTextStyle.weightExtraBold
+                : NewThemeTextStyle.weightMedium,
+          ).applyShaders(context),
+          isSelected
+              ? CircleAvatar(
+                  radius: 7,
+                  backgroundColor: ThemeColor.cicloColor,
+                  child: Padding(
+                    padding: const EdgeInsets.all(
+                      1.0,
+                    ),
+                    child: SvgPicture.asset(
+                      ThemeIcon.checkMark,
+                    ),
+                  ),
+                )
+              : Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: GradientBoxBorder(
+                      gradient: ThemeGradient.primary,
+                      width: 1,
+                    ),
+                  ),
+                ),
+        ],
+      ),
+    );
+  }
+}
