46eec0558dd561d404e84d803151965c55b4bd2f
Danilo Spanò <d.spano@tandu.it>
2024-01-30 09:27:05 +0100

    feat: lazyput in binding

--- lib/modules/register/register_binding.dart
+++ lib/modules/register/register_binding.dart
@@ -4,6 +4,6 @@ import 'package:lines/modules/register/register_controller.dart';
 class RegisterBinding implements Bindings {
   @override
   void dependencies() {
-    Get.put(RegisterAndLoginController());
+    Get.lazyPut(() => RegisterAndLoginController());
   }
 }
