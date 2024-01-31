ee846c0abb29d780960fe0c71fcba33bccd8c0ee
Danilo Spanò <d.spano@tandu.it>
2024-01-31 15:56:37 +0100

    fix: fixe check_email.dart model

--- lib/data/models/check_email.dart
+++ lib/data/models/check_email.dart
@@ -1,26 +1,26 @@
 class CheckEmail {
   /// True if the email is already registered
-  bool emailExists;
+  bool? emailExists;
 
   /// True if the user has already completed the email verification
-  bool emailIsActive;
+  bool? emailIsActive;
 
   CheckEmail({
-    required this.emailExists,
-    required this.emailIsActive,
+    this.emailExists,
+    this.emailIsActive,
   });
 
   factory CheckEmail.fromJson(Map<String, dynamic> json) {
     return CheckEmail(
-      emailExists: json['email_exists'],
-      emailIsActive: json['email_is_active'],
+      emailExists: json['exists'],
+      emailIsActive: json['active'],
     );
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = {};
-    data['email_exists'] = emailExists;
-    data['email_is_active'] = emailIsActive;
+    data['exists'] = emailExists;
+    data['active'] = emailIsActive;
     return data;
   }
 }
