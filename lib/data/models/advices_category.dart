d3a10d5d422c280296d5acae95fb92b58aa7b2a8
Giacomo Damosso <g.damosso@tandu.it>
2024-02-05 13:52:32 +0000

    Merged in feature/advice_category_model (pull request #66)
    feat : added advices category model (use this class instead of enum later)
    
    * added advice category model
    
    * formatted code
    
    * changed field name
    
    * changed import statement
    
    * Merged develop into feature/advice_category_model
    
    Approved-by: Danilo Spanò

---
+++ lib/data/models/advices_category.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import '../../core/app_theme.dart';
+
+class AdvicesCategory {
+  final String iconName;
+  final String categoryTitle;
+
+  AdvicesCategory({
+    required this.iconName,
+    required this.categoryTitle,
+  });
+
+  factory AdvicesCategory.fromJson(Map<String, dynamic> data) {
+    return AdvicesCategory(
+      iconName: data['icon_name'],
+      categoryTitle: data['name'],
+    );
+  }
+
+  Color get categoryColor {
+    switch (iconName) {
+      case 'ginecologia.svg':
+        return ThemeColor.ginecologiaColor;
+      case 'sessualita_e_fertilita.svg':
+        return ThemeColor.sessualitaEFertilitaColor;
+      case 'tabu_e_inclusivita.svg':
+        return ThemeColor.tabuEInclusivitaColor;
+      case 'fitness_and_wellness.svg':
+        return ThemeColor.fitnessAndWellnessColor;
+      case 'beauty_and_lifestyle.svg':
+        return ThemeColor.beautyAndLifestyleColor;
+      case 'alimentazione.svg':
+        return ThemeColor.alimentazioneColor;
+      case 'benessere_emotivo.svg':
+        return ThemeColor.benessereEmotivoColor;
+      case 'lines_al_tuo_fianco.svg':
+        return ThemeColor.linesAlTuoFiancoColor;
+      default:
+        return Colors.white;
+    }
+  }
+
+  String get iconPath {
+    switch (iconName) {
+      case 'ginecologia.svg':
+        return ThemeIcon.ginecologia;
+      case 'sessualita_e_fertilita.svg':
+        return ThemeIcon.sessualitaEFertilita;
+      case 'tabu_e_inclusivita.svg':
+        return ThemeIcon.tabueEInclusivita;
+      case 'fitness_and_wellness.svg':
+        return ThemeIcon.fitnessAndWellness;
+      case 'beauty_and_lifestyle.svg':
+        return ThemeIcon.beautyAndLifestyle;
+      case 'alimentazione.svg':
+        return ThemeIcon.alimentazione;
+      case 'benessere_emotivo.svg':
+        return ThemeIcon.benessereEmotivo;
+      case 'lines_al_tuo_fianco.svg':
+        return ThemeIcon.linesAlTuoFianco;
+      default:
+        return '';
+    }
+  }
+}
