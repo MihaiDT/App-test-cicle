a3b81b1e25e01965ff93a5643cc040965f83eaa0
Giacomo Damosso <g.damosso@tandu.it>
2024-02-05 13:53:02 +0000

    Merged in feature/advices_article_model (pull request #68)
    feat : created advice article model
    
    * created advice article model
    
    * fixed list init model
    
    * added late init
    
    * added toJson method
    
    * formatted code
    
    
    Approved-by: Danilo Spanò

---
+++ lib/data/models/advices_article.dart
@@ -0,0 +1,142 @@
+class AdvicesArticle {
+  final String articleCategoryId;
+  final int id;
+  final ArticleType typology;
+  final String title;
+  final String? disclaimer;
+  late final String? coverImageUrl;
+  late final List<String>? slideshowImageUrls;
+  late final String? shortDescription;
+  late final String? text;
+  late final String? thumbImageUrl;
+  late final String? videoImagePreview;
+  late final String? videoUrl;
+
+  AdvicesArticle.textType({
+    required this.articleCategoryId,
+    required this.id,
+    required this.typology,
+    required this.title,
+    required this.disclaimer,
+    required this.coverImageUrl,
+    required this.shortDescription,
+    required this.text,
+    required this.thumbImageUrl,
+  }) {
+    videoUrl = null;
+    videoImagePreview = null;
+    slideshowImageUrls = null;
+  }
+
+  AdvicesArticle.sliderType({
+    required this.articleCategoryId,
+    required this.id,
+    required this.typology,
+    required this.title,
+    required this.disclaimer,
+    required this.coverImageUrl,
+    required this.thumbImageUrl,
+    required this.slideshowImageUrls,
+  }) {
+    videoUrl = null;
+    videoImagePreview = null;
+    text = null;
+    shortDescription = null;
+  }
+
+  AdvicesArticle.videoType({
+    required this.articleCategoryId,
+    required this.id,
+    required this.typology,
+    required this.title,
+    required this.disclaimer,
+    required this.thumbImageUrl,
+    required this.videoImagePreview,
+    required this.videoUrl,
+  }) {
+    text = null;
+    shortDescription = null;
+    slideshowImageUrls = null;
+    coverImageUrl = null;
+  }
+
+  factory AdvicesArticle.fromJson(Map<String, dynamic> data) {
+    ArticleType articleType = ArticleType.fromString(data['typology']);
+    switch (articleType) {
+      case ArticleType.text:
+        return AdvicesArticle.textType(
+          articleCategoryId: data['article_category_id'],
+          id: data['id'],
+          typology: articleType,
+          title: data['title'],
+          disclaimer: data['disclaimer'],
+          coverImageUrl: data['cover_image_ur'],
+          shortDescription: data['short_description'],
+          text: data['text'],
+          thumbImageUrl: data['thumb_image_url'],
+        );
+      case ArticleType.slider:
+        return AdvicesArticle.sliderType(
+          articleCategoryId: data['article_category_id'],
+          id: data['id'],
+          typology: articleType,
+          title: data['title'],
+          disclaimer: data['disclaimer'],
+          coverImageUrl: data['cover_image_ur'],
+          thumbImageUrl: data['thumb_image_url'],
+          slideshowImageUrls: List<String>.from(data['slideshow_image_urls']),
+        );
+      case ArticleType.video:
+        return AdvicesArticle.videoType(
+          articleCategoryId: data['article_category_id'],
+          id: data['id'],
+          typology: articleType,
+          title: data['title'],
+          disclaimer: data['disclaimer'],
+          thumbImageUrl: data['thumb_image_url'],
+          videoImagePreview: data['video_image_preview'],
+          videoUrl: data['video_url'],
+        );
+      default:
+        throw Exception("Article can't have undefinedType");
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> json = {
+      'article_category_id': articleCategoryId,
+      'id': id,
+      'typology': typology.name,
+      'title': title,
+      'disclaimer': disclaimer,
+      'cover_image_url': coverImageUrl,
+      'slideshow_image_urls': slideshowImageUrls,
+      'short_description': shortDescription,
+      'text': text,
+      'thumb_image_url': thumbImageUrl,
+      'video_image_preview': videoImagePreview,
+      'video_url': videoUrl,
+    };
+    return json;
+  }
+}
+
+enum ArticleType {
+  text,
+  slider,
+  video,
+  undefinedType;
+
+  static ArticleType fromString(String type) {
+    switch (type) {
+      case 'text':
+        return ArticleType.text;
+      case 'slider':
+        return ArticleType.slider;
+      case 'video':
+        return ArticleType.video;
+      default:
+        return ArticleType.undefinedType;
+    }
+  }
+}
