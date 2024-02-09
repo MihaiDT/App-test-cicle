3c7d4e1d907a39d18e55801c2b4a25df11b4c33d
Giacomo Damosso <g.damosso@tandu.it>
2024-02-09 13:13:21 +0000

    Merged in feature/text_article_ui_changes (pull request #89)
    feat : changed appbar behavior when scrolling
    
    * formatted code
    
    * formatted code
    
    * Merge branch 'feature/article_text_detail' into feature/article_slider_detail
    
    * Merge branch 'feature/article_slider_detail' into feature/article_video_detail
    
    * changed to sliverappbar
    
    * changed sliver appbar layout
    
    * changed to dynamic opacity
    
    * Merge branch 'develop' into feature/text_article_ui_changes
    
    # Conflicts:
    #	lib/modules/advices/advices_detail_page.dart
    #	lib/modules/advices/widgets/advices_slider_article_detail.dart
    #	lib/modules/advices/widgets/advices_text_article_detail.dart
    #	lib/modules/advices/widgets/advices_video_article_detail.dart
    #	pubspec.lock
    
    * removed print
    
    * refactoring
    
    
    Approved-by: Danilo Spanò

--- lib/modules/advices/widgets/advices_text_article_detail.dart
+++ lib/modules/advices/widgets/advices_text_article_detail.dart
@@ -7,134 +7,168 @@ import 'package:lines/widgets/layouts/app_scaffold_page.dart';
 
 import '../../../data/models/advices_article.dart';
 
-class AdvicesTextArticleDetails extends StatelessWidget {
+class AdvicesTextArticleDetails extends StatefulWidget {
   final AdvicesArticle article;
   final AdvicesCategory category;
 
   const AdvicesTextArticleDetails({
     required this.article,
-    required this.category,
     super.key,
+    required this.category,
   });
 
-  static Color dividerColor = const Color(0x194b399c);
+
+
+  @override
+  State<AdvicesTextArticleDetails> createState() =>
+      _AdvicesTextArticleDetailsState();
+}
+
+class _AdvicesTextArticleDetailsState extends State<AdvicesTextArticleDetails> {
+  final ScrollController _scrollController = ScrollController();
+  double _proportion = 0;
+
+
+  static const Color _dividerColor = Color(0x194b399c);
+  static const Color _disclaimerColor = Color(0x7f1f2d4f);
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      _proportion = _scrollController.offset /
+          _scrollController.position.viewportDimension;
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     AdvicesCategory categoryFromArticle = AdvicesCategory.fromIconName(
-      article.iconName,
+      widget.article.iconName,
     );
 
     return AppScaffoldPage(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Stack(
-          children: [
-            ListView(
-              padding: const EdgeInsetsDirectional.symmetric(
-                horizontal: 32,
-              ),
-              children: [
-                const SizedBox(
-                  height: 260,
-                ),
-                ThemeSizedBox.height32,
-                CircleAvatar(
-                  backgroundColor: categoryFromArticle.categoryColor,
-                  child: SvgPicture.asset(
-                    categoryFromArticle.iconPath,
-                    color: Colors.white,
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          _appBar(context),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 32,
+            ),
+            sliver: SliverList(
+              delegate: SliverChildListDelegate(
+                <Widget>[
+                  ThemeSizedBox.height32,
+                  CircleAvatar(
+                    backgroundColor: categoryFromArticle.categoryColor,
+                    child: SvgPicture.asset(
+                      categoryFromArticle.iconPath,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                TitleMedium(
-                  category.categoryTitle != null
-                      ? category.categoryTitle!.toUpperCase()
-                      : "",
-                  color: ThemeColor.darkBlue,
-                  textAlign: TextAlign.center,
-                ),
-                ThemeSizedBox.height4,
-                DisplayMedium(
-                  article.title,
-                  textAlign: TextAlign.center,
-                ).applyShaders(context),
-                ThemeSizedBox.height16,
-                BodyLarge(
-                  article.shortDescription ?? "",
-                  color: ThemeColor.darkBlue,
-                  textAlign: TextAlign.center,
-                  fontWeight: FontWeight.w500,
-                ),
-                ThemeSizedBox.height48,
-                Divider(
-                  color: dividerColor,
-                ),
-                ThemeSizedBox.height48,
-                Html(
-                  data: article.text,
-                ),
-              ],
+                  ThemeSizedBox.height8,
+                  TitleMedium(
+                    widget.category.categoryTitle?.toUpperCase() ?? "",
+                    color: ThemeColor.darkBlue,
+                    textAlign: TextAlign.center,
+                  ),
+                  ThemeSizedBox.height4,
+                  DisplayMedium(
+                    widget.article.title,
+                    textAlign: TextAlign.center,
+                  ).applyShaders(context),
+                  ThemeSizedBox.height16,
+                  BodyLarge(
+                    widget.article.shortDescription ?? "",
+                    color: ThemeColor.darkBlue,
+                    textAlign: TextAlign.center,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  ThemeSizedBox.height48,
+                  const Divider(
+                    color: _dividerColor,
+                  ),
+                  ThemeSizedBox.height48,
+                  Html(
+                    data: widget.article.text,
+                  ),
+                  ThemeSizedBox.height48,
+                  LabelLarge(
+                    widget.article.disclaimer ?? "",
+                    color: _disclaimerColor,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  ThemeSizedBox.height60,
+                ],
+              ),
             ),
-            SizedBox(
-              height: 260,
-              child: _topBar(context),
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
 
-  Widget _topBar(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        image: article.thumbImageUrl?.isNotEmpty == true
-            ? DecorationImage(
-                image: NetworkImage(
-                  article.thumbImageUrl!,
-                ),
-                fit: BoxFit.cover,
-              )
-            : null,
-        color: article.thumbImageUrl?.isEmpty == true
-            ? ThemeColor.primary
-            : Colors.transparent,
+  Widget _appBar(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: widget.category.categoryColor,
+      elevation: 0,
+      leading: InkWell(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: SvgPicture.asset(
+            ThemeIcon.back,
+            color: ThemeColor.darkBlue,
+          ),
+        ),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      expandedHeight: 375,
+      floating: false,
+      pinned: true,
+      snap: false,
+      flexibleSpace: Stack(
         children: [
-          Align(
-            alignment: Alignment.topLeft,
-            child: InkWell(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: SvgPicture.asset(
-                ThemeIcon.back,
-                color: ThemeColor.darkBlue,
+          Visibility(
+            visible: widget.article.thumbImageUrl?.isNotEmpty == true,
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 500),
+              opacity: _proportion <= 0.40 ? 1.0 : 0.0,
+              child: Image.network(
+                widget.article.thumbImageUrl!,
+                fit: BoxFit.cover,
+                width: double.maxFinite,
               ),
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              CircleAvatar(
-                radius: 16,
-                backgroundColor: Colors.white,
-                child: SvgPicture.asset(
-                  ThemeIcon.shareGradient,
-                ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  CircleAvatar(
+                    radius: 16,
+                    backgroundColor: Colors.white,
+                    child: SvgPicture.asset(
+                      ThemeIcon.shareGradient,
+                    ),
+                  ),
+                  ThemeSizedBox.width8,
+                  CircleAvatar(
+                    radius: 16,
+                    backgroundColor: Colors.white,
+                    child: SvgPicture.asset(
+                      ThemeIcon.savedEmptyGradient,
+                    ),
+                  ),
+                ],
               ),
-              ThemeSizedBox.width8,
-              CircleAvatar(
-                radius: 16,
-                backgroundColor: Colors.white,
-                child: SvgPicture.asset(
-                  ThemeIcon.savedEmptyGradient,
-                ),
-              ),
-            ],
+            ),
           )
         ],
       ),
