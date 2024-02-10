de114cab95f413024732243ea9a822ebe364a773
Marco Fiore <m.fiore@tandu.it>
2024-02-10 15:27:22 +0100

    Added  follicular and luteal phases to PeriodPhase

--- lib/data/enums/period_phase.dart
+++ lib/data/enums/period_phase.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:lines/core/app_theme.dart';
+
 enum PeriodPhase {
+  follicular,
+  luteal,
   menstruation,
   ovulation,
   noPhase;
@@ -9,8 +14,27 @@ enum PeriodPhase {
         return PeriodPhase.menstruation;
       case 'ovulation':
         return PeriodPhase.ovulation;
+      case 'follicular':
+        return PeriodPhase.follicular;
+      case 'luteal':
+        return PeriodPhase.luteal;
       default:
         return PeriodPhase.noPhase;
+    }
+  }
+
+  Color get periodColor {
+    switch (this) {
+      case PeriodPhase.menstruation:
+        return ThemeColor.menstruationColor;
+      case PeriodPhase.ovulation:
+        return ThemeColor.ovulationColor;
+      case PeriodPhase.follicular:
+        return ThemeColor.follicularColor;
+      case PeriodPhase.luteal:
+        return ThemeColor.lutealColor;
+      default:
+        return ThemeColor.defaultPeriodColor;
     }
   }
 }
