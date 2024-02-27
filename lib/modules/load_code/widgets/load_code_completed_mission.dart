d753c084686971c8ad265373636ee30577b6c636
Giacomo Damosso <g.damosso@tandu.it>
2024-02-27 15:48:29 +0000

    Merged in feature/load_code_results_completed_mission (pull request #141)
    feat : added completed mission section for load code page
    
    * added gradient checkmark
    
    * changed style
    
    * added elevated card to mission card
    
    * removed non used page
    
    * comma
    
    * added header
    
    * Merge branch 'feature/selectable_mission_container' into feature/load_code_results_completed_mission
    
    * added completed mission widget
    
    * formatted code
    
    
    Approved-by: Danilo Spanò

---
+++ lib/modules/load_code/widgets/load_code_completed_mission.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lines/core/app_theme.dart';
+import 'package:lines/data/models/mission.dart';
+import 'package:lines/modules/load_code/widgets/load_code_mission_header.dart';
+import 'package:lines/modules/prizes/widgets/mission_container.dart';
+
+class LoadCodeCompletedMission extends StatelessWidget {
+  const LoadCodeCompletedMission({
+    super.key,
+  });
+
+  double get _missionContainerSize => Get.height * 0.35;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: ThemeSize.paddingLarge,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const LoadCodeMissionHeader(
+            title: "Hai completato una missione",
+            description:
+                "Questo codice prodotto è valido per la seguente missione:",
+          ),
+          ThemeSizedBox.height16,
+          SizedBox(
+            height: _missionContainerSize,
+            child: MissionContainer(
+              mission: Mission(
+                currentCodes: 3,
+                description:
+                    "Acquista 3 prodotti Lines di cui almeno 1 notte /extra, riceverai un buono sconto su Zalando di 5€",
+                totalCodes: 3,
+                untilDate: "31/10/23",
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
