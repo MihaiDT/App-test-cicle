c4db346190d5096c4bb8b63cb67dc60031c8bd96
Giacomo Damosso <g.damosso@tandu.it>
2024-02-28 16:23:38 +0000

    Merged in feature/missions_how_to_participate_step_two (pull request #149)
    feat : added step two container of how to partecipate section
    
    * added second step
    
    * removed text
    
    * changed folder name
    
    * Merged develop into feature/missions_how_to_participate_step_two
    * changed height values
    
    
    Approved-by: Danilo Spanò

---
+++ lib/modules/missions/widgets/missions_how_to_participate_step_two.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:lines/core/app_theme.dart';
+import 'package:lines/modules/missions/widgets/missions_how_to_participate_header.dart';
+
+
+class MissionsHowToParticipateStepTwo extends StatelessWidget {
+  const MissionsHowToParticipateStepTwo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: ThemeSize.paddingMedium,
+      ),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ThemeSizedBox.height20,
+          const MissionsHowToParticipateHeader(
+            stepNumber: 2,
+            description:
+                "Carica i codici prodotto che trovi stampati all'interno delle confezioni. ",
+          ),
+          ThemeSizedBox.height8,
+          const BodyMedium(
+            "Tranquilla, puoi caricarli anche in momenti diversi.",
+            color: ThemeColor.darkBlue,
+            textAlign: TextAlign.center,
+          ),
+          ThemeSizedBox.height16,
+          Image.asset(
+            ThemeImage.findingCode,
+            height: 100,
+          ),
+          ThemeSizedBox.height20,
+        ],
+      ),
+    );
+  }
+}
