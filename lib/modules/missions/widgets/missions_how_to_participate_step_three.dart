2368099ecb622dab582c6775d7d32659c26de1d8
Giacomo Damosso <g.damosso@tandu.it>
2024-02-28 16:38:09 +0000

    Merged in feature/missions_how_to_participate_step_three (pull request #150)
    feat : added step three container of how to partecipate section
    
    * added step three file
    
    * finished widget
    
    * changed directory location
    
    * changed height values
    
    
    Approved-by: Danilo Spanò

---
+++ lib/modules/missions/widgets/missions_how_to_participate_step_three.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:lines/core/app_theme.dart';
+import 'package:lines/modules/missions/widgets/missions_how_to_participate_header.dart';
+
+class MissionsHowToParticipateStepThree extends StatelessWidget {
+  const MissionsHowToParticipateStepThree({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ThemeSizedBox.height20,
+          const MissionsHowToParticipateHeader(
+            stepNumber: 3,
+            description: "Ricevi subito il tuo premio",
+          ),
+          ThemeSizedBox.height16,
+          const Center(
+            child: SizedBox(
+              height: 100,
+              width: 160,
+              child: Placeholder(),
+            ),
+          ),
+          ThemeSizedBox.height20,
+        ],
+      ),
+    );
+  }
+}
