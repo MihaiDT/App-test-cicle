6951f90b137874aee2ee925fc4bff4331ab74b03
Giacomo Damosso <g.damosso@tandu.it>
2024-03-01 09:13:19 +0000

    Merged in feature/invite_a_friend_code (pull request #153)
    feat : added invite a friend code widget
    
    * added widget
    
    * fix
    
    
    Approved-by: Danilo Spanò

---
+++ lib/modules/invite_friend/widgets/invite_friend_code.dart
@@ -0,0 +1,58 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:lines/core/app_theme.dart';
+
+class InviteFriendCode extends StatelessWidget {
+  final String code;
+
+  const InviteFriendCode({
+    required this.code,
+    super.key,
+  });
+
+  static const Color _bgColor = Color(0xfff3eef4);
+  static const Color _borderColor = Color(0xffe5e1ef);
+  static const double _borderRadius = 20.0;
+  static const double _borderPadding = 4.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(
+        _borderPadding,
+      ),
+      decoration: const BoxDecoration(
+        color: _bgColor,
+        borderRadius: BorderRadius.all(
+          Radius.circular(
+            _borderRadius,
+          ),
+        ),
+      ),
+      child: DottedBorder(
+        strokeWidth: 1.5,
+        dashPattern: const [6, 6],
+        borderType: BorderType.RRect,
+        radius: const Radius.circular(
+          _borderRadius - _borderPadding,
+        ),
+        color: _borderColor,
+        padding: const EdgeInsets.symmetric(
+          horizontal: ThemeSize.paddingMedium,
+          vertical: ThemeSize.paddingSmall,
+        ),
+        child: Center(
+          child: Text(
+            code,
+            style: const TextStyle(
+              color: ThemeColor.primary,
+              fontSize: 30,
+              letterSpacing: 4,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
