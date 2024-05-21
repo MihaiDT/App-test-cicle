14e01685b60e22b07520499b8acc0da31e160844
Danilo Spanò <d.spano@tandu.it>
2024-05-21 09:18:33 +0200

    feat: added sharing of mgmcode

--- lib/modules/invite_friend/invite_friend_controller.dart
+++ lib/modules/invite_friend/invite_friend_controller.dart
@@ -8,7 +8,8 @@ class InviteFriendController extends GetxController {
   Future<void> shareCode() async {
     await FlutterShare.share(
       title: 'Condividi il codice amico',
-      text: 'Ciao! Usa il mio codice amico per registrarti in app: ERKDEL56',
+      text:
+          'Ciao! Usa il mio codice amico per registrarti in app: $referralCode',
     );
   }
 }
