885c4f28303f175d6741564d1b2d77debee463d1
Marco Fiore <m.fiore@tandu.it>
2024-06-18 16:38:24 +0200

    Update section using obx

--- lib/modules/profile/widgets/my_menses_section.dart
+++ lib/modules/profile/widgets/my_menses_section.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:lines/core/app_theme.dart';
+import 'package:lines/core/utils/singletons.dart';
 import 'package:lines/modules/profile/controllers/my_menses_section_controller.dart';
 import 'package:lines/routes/routes.dart';
 import 'package:lines/widgets/cards/elevated_card.dart';
@@ -35,56 +36,65 @@ class MyMensesSection extends GetView<MyMensesSectionController> {
                       color: ThemeColor.darkBlue,
                     ),
               ThemeSizedBox.height16,
-              if (controller.hasSavedMensesInfo) ...[
-                Row(
-                  children: [
-                    _infoCard(
-                      value: controller.periodDays.toString(),
-                      description: "Durata media flusso",
-                      imagePath: ThemeIcon.dropRoundedIcon,
-                    ),
-                    ThemeSizedBox.width8,
-                    _infoCard(
-                      value: controller.periodDuration.toString(),
-                      description: "Durata media ciclo",
-                      imagePath: ThemeIcon.circleRoundedIcon,
-                    ),
-                  ],
-                ),
-                ThemeSizedBox.height8,
-                InkWell(
-                  onTap: () {
-                    Get.toNamed(Routes.yourMensesStatsPage);
-                  },
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(60),
-                      ),
-                      color: Colors.white,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(
-                        16,
-                      ),
-                      child: Row(
-                        children: [
-                          const TitleLarge(
-                            "GRAFICI E STATISTICHE",
-                            letterSpacing: 2,
-                          ).applyShaders(context),
-                          const Spacer(),
-                          SvgPicture.asset(
-                            ThemeIcon.arrowRight,
-                            color: ThemeGradient.colorPrimaryGradientLight,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                ThemeSizedBox.height24,
-              ],
+              Obx(
+                () =>
+                    (appController.currentPeriod.responseHandler.isSuccessful &&
+                            controller.hasSavedMensesInfo)
+                        ? Column(
+                            children: [
+                              Row(
+                                children: [
+                                  _infoCard(
+                                    value: controller.periodDays.toString(),
+                                    description: "Durata media flusso",
+                                    imagePath: ThemeIcon.dropRoundedIcon,
+                                  ),
+                                  ThemeSizedBox.width8,
+                                  _infoCard(
+                                    value: controller.periodDuration.toString(),
+                                    description: "Durata media ciclo",
+                                    imagePath: ThemeIcon.circleRoundedIcon,
+                                  ),
+                                ],
+                              ),
+                              ThemeSizedBox.height8,
+                              InkWell(
+                                onTap: () {
+                                  Get.toNamed(Routes.yourMensesStatsPage);
+                                },
+                                child: Container(
+                                  decoration: const BoxDecoration(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(60),
+                                    ),
+                                    color: Colors.white,
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(
+                                      16,
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        const TitleLarge(
+                                          "GRAFICI E STATISTICHE",
+                                          letterSpacing: 2,
+                                        ).applyShaders(context),
+                                        const Spacer(),
+                                        SvgPicture.asset(
+                                          ThemeIcon.arrowRight,
+                                          color: ThemeGradient
+                                              .colorPrimaryGradientLight,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              ThemeSizedBox.height24,
+                            ],
+                          )
+                        : const SizedBox.shrink(),
+              ),
             ],
           ),
         ),
